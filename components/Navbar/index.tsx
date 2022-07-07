277a8e2396c73085f5d2b7b568ba4ea9e936ebf4
kcshan <cshan.kong@gmail.com>
2022-07-07 10:24:54 +0800

    fix

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Button } from 'antd';
+import styles from './index.module.scss';
+import { navs } from './config';
+import Login from 'components/Login';
+
+const Home: NextPage = () => {
+  const { pathname } = useRouter();
+  const [isShowLogin, setIsShowLogin] = useState(false);
+
+  const handleGotoEditorPage = () => {};
+
+  const handleLogin = () => {
+    setIsShowLogin(true);
+  };
+
+  const handleClose = () => {
+    setIsShowLogin(false);
+  };
+
+  return (
+    <div className={styles.navbar}>
+      <section className={styles.logoArea}>BLOG-C</section>
+      <section className={styles.linkArea}>
+        {navs?.map((nav) => (
+          <Link key={nav?.label} href={nav?.value}>
+            <a className={pathname === nav?.value ? styles.active : ''}>
+              {nav?.label}
+            </a>
+          </Link>
+        ))}
+      </section>
+      <section className={styles.operationArea}>
+        <Button onClick={handleGotoEditorPage}>写文章</Button>
+        <Button type="primary" onClick={handleLogin}>
+          登录
+        </Button>
+      </section>
+      <Login isShow={isShowLogin} onClose={handleClose} />
+    </div>
+  );
+};
+
+export default Home;
